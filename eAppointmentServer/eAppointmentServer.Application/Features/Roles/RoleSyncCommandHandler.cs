c4426b4c57f1fcdca82972754816c08974de5981
Melih Can Akgüneş <melihcanakgunes@gmail.com>
2024-10-14 17:02:43 +0300

    user roles added

---
+++ eAppointmentServer/eAppointmentServer.Application/Features/Roles/RoleSyncCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using eAppointmentServer.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Features.Roles;
+
+internal sealed class RoleSyncCommandHandler(
+    RoleManager<AppRole> roleManager) : IRequestHandler<RoleSyncCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(RoleSyncCommand request, CancellationToken cancellationToken)
+    {
+        List<AppRole> currentRoles = await roleManager.Roles.ToListAsync(cancellationToken);
+
+        List<AppRole> staticRoles = Constants.GetRoles();
+
+        foreach (var role in currentRoles)
+        {
+            if (!staticRoles.Any(s => s.Name == role.Name))
+            {
+                await roleManager.DeleteAsync(role);
+            }
+        }
+
+        foreach (var role in staticRoles)
+        {
+            if (!currentRoles.Any(s => s.Name == role.Name))
+            {
+                await roleManager.CreateAsync(role);
+            }
+        }
+
+        return "Sync is successfull!";
+    }
+}
---
+++ eAppointmentServer/eAppointmentServer.WebAPI/Controllers/RoleController.cs
@@ -0,0 +1,22 @@
+﻿using eAppointmentServer.Application.Features.Roles;
+using eAppointmentServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eAppointmentServer.WebAPI.Controllers;
+
+[AllowAnonymous]
+public sealed class RoleController : ApiController
+{
+    public RoleController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Sync(RoleSyncCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+}
